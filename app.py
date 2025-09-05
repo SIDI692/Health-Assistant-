d612fcfd8f28dafe88da1a2192f7d9fcf46a463f
SIDI692 <sidialassane798@gmail.com>
2025-09-05 23:50:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -133,3 +133,6 @@ def reset():
 if __name__ == "__main__":
     app.run(debug=True)
 
+imc_value = poids / (taille ** 2)
+if imc_value < 10 or imc_value > 70:
+    raise ValueError("Valeur d'IMC irréaliste")
